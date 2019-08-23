d74fe263f2952c6a96bddf3c99cb38c72f30f45e
Nick <nicholastysh@gmail.com>
2019-08-23 12:42:28 -0400

    added dbSeed folder, dynamic backgrounds, buttons instead of radios, working page paths!

--- routes/api.js
+++ routes/api.js
@@ -3,21 +3,24 @@ const express = require('express')
 const router = express.Router()
 
 router
-  .get('/:level', (req, res) => {
+  .get('/game/:level', (req, res) => {
 
     req.connection.query('SELECT * FROM level_questions WHERE id = ?; SELECT * FROM player WHERE id = ?', [req.params.level, 1], (err, data) => {
         
           // display json so can grab data using ajax
           // return res.json(question[1][0].id)
-          // res.json(question[0])
+          // res.json(data[0])
           // res.json(stats[0])
    
          
           res.render('index', {
             //question
+
+            qId: data[0][0].id,
             question: data[0][0].question,
             choices: data[0][0].choices,
-            qId: data[0][0].id,
+            page: data[0][0].page_number,
+            background: data[0][0].image_path,
             // stats
             name: data[1][0].player_name,
             health: data[1][0].player_health
