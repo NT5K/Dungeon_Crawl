4dcc4354dca5ddc297767828e1960ac53aa8306b
Nick <nicholastysh@gmail.com>
2019-08-28 11:35:51 -0400

    remove unused backgrounds, unused paths

--- routes/api.js
+++ routes/api.js
@@ -181,37 +181,7 @@ router
     return result.send(req.session)
 
   });
-=======
-const express = require('express')
-
-const router = express.Router()
-
-router
-  .get('/:level', (req, res) => {
-
-    req.connection.query('SELECT * FROM level_questions WHERE id = ?; SELECT * FROM player WHERE id = ?', [req.params.level, req.params.level], (err, data) => {
-        
-          // display json so can grab data using ajax
-          // return res.json(question[1][0].id)
-          // res.json(question[0])
-          // res.json(stats[0])
-   
-         
-          res.render('index', {
-            //question
-            question: data[0][0].question,
-            choices: data[0][0].choices,
-            qId: data[0][0].id,
-            // stats
-            name: data[1][0].player_name,
-            health: data[1][0].player_health
-          })
-        
-        
-      })
-    })           
-
-
+      
 module.exports = router
 
 // router
--- routes/html.js
+++ routes/html.js
@@ -2,12 +2,6 @@ const express = require('express')
 const path = require('path');
 const router = express.Router()
 
-// router.get('/', (_, res) => {
-//   res.render('index', {
-//     title: 'My Cool App',
-//     user: 'Nerd'
-//   })
-// })
 router.get('/', (_, res) => {
   res.render('startscreen')
 })
